031f4399ed290d190d6f088477b36e5ea2f8595e
ue4prog <vladtet2000@mail.ru>
2020-02-25 19:36:05 +0300

    feat : jobs done!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) {  
+    if (!array || array.length === 0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    return array.reduce((acc, curr) => acc += curr, 0) / array.length;
 }
